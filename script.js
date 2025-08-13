2403f40563edd2b3b416eaa0ef9421526d3d6e2c
Giga-Digi <oliver.lindkvist@me.com>
2025-08-13 10:00:30 +0200

    up

--- script.js
+++ script.js
@@ -54,12 +54,7 @@ function loadProject(projectName) {
     viewer.innerHTML = `
       <div class="video-wrapper">
         <div style="padding:76.06% 0 0 0;position:relative;">
-          <iframe src="https://player.vimeo.com/video/1109333539?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            style="position:absolute;top:0;left:0;width:100%;height:100%;"
-            title="PIZZERIA ROMA"></iframe>
+          <iframe src="https://player.vimeo.com/video/1109333539?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1000" height="761" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" title="PIZZERIA ROMA"></iframe>
         </div>
       </div>
       <div class="video-description">
